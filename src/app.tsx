852024d11320f6c0f7b384559f3d72d9ea4784ce
Nath <playerxmatt@gmail.com>
2024-02-20 16:25:52 -0300

    feat: added main structure

--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,11 @@
 export function App() {
-  return <div className="py-10 space-y-8"></div>;
+  return (
+    <div className="py-10 space-y-8">
+      <div>Header tabs</div>
+      <main className="max-w-6xl mx-auto space-y-5">
+        <h1 className="text-xl font-bold">Tags</h1>
+        <button>Create new</button>
+      </main>
+    </div>
+  );
 }
