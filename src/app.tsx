4819cb437cdf24c06e8a9f59f59d73b098c00623
Nath <playerxmatt@gmail.com>
2024-02-22 14:28:50 -0300

    feat: added create tag form to home page modal

--- src/app.tsx
+++ src/app.tsx
@@ -16,6 +16,7 @@ import { Pagination } from "./components/pagination";
 import { useSearchParams } from "react-router-dom";
 import { useState } from "react";
 import * as Dialog from "@radix-ui/react-dialog";
+import { CreateTagForm } from "./components/create-tag-form";
 
 export interface TagResponse {
   first: number;
@@ -96,6 +97,8 @@ export function App() {
                     Tags can be used to group videos about similar concepts
                   </Dialog.Description>
                 </div>
+
+                <CreateTagForm />
 
                 <Dialog.Close />
               </Dialog.Content>
